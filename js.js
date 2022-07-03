afd583fdb74aba6748fb85208682869cfb7cc85d
abo-habeba <94226077+abo-habeba@users.noreply.github.com>
2022-07-03 14:51:48 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -56,3 +56,9 @@ settings.classList.toggle("open")
 });
 
 
+// hts is  my footer
+let myFooter =document.createElement('footer');
+document.body.appendChild(myFooter);
+myFooter.innerText = 'By Hesham Mohamed © 2022';
+myFooter.style.cssText = 'background-color: dimgray; position:fixed; bottom:0px; width: 100%; height:50px; margin:0px; padding:0px; text-align:center; color:white;display: flex;align-items:center;justify-content: center';
+
