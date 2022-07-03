b44eef660fbf3c1d62d8406a3c86be8e401bd805
abo-habeba <94226077+abo-habeba@users.noreply.github.com>
2022-07-03 14:11:27 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,42 +1,58 @@
+localStorage.setItem("rate", "");
+onload = function(){
+  if (localStorage.getItem('rate') == "") {
+    document.getElementById('input4').value = 27;
+  } else{
+    document.getElementById('input4').value = localStorage.getItem('rate');
+  };
+  };
+function inputs(){
+var num1  =  document.getElementById('input1').value; //سعر المنتج كاش
+var num2  =  document.getElementById('input2').value; //المقدم
+var num3  =  document.getElementById('input3').value; //عدد الشهور
+var num4  =  document.getElementById('input4').value; //النسبة
+var num5  =  document.getElementById("input5");       //الباقي بعد المقدم
+var num6  =  document.getElementById("input6");       //نسبة اجمالي الشهور
+var num7  =  document.getElementById("input7");       //نسبة الربح بالجنية
+var num8  =  document.getElementById("input8");       // سعر المنتج قسط 0.0
+var num9  =  document.getElementById("input9");       //المبلغ المقسط
+var num10 =  document.getElementById("input10");      //القسط الشهري
+var num11 =  document.getElementById("input11");      //سعر المنتج قسط
 
-        function inputs()
-              {
-                var num1  =  document.getElementById('input1').value; //سعر المنتج كاش
-                var num2  =  document.getElementById('input2').value; //المقدم
-                var num3  =  document.getElementById('input3').value; //عدد الشهور
-                var num4  =  document.getElementById('input4').value; //النسبة
-                var num5  =  document.getElementById("input5");       //الباقي بعد المقدم
-                var num6  =  document.getElementById("input6");       //نسبة اجمالي الشهور
-                var num7  =  document.getElementById("input7");       //نسبة الربح بالجنية
-                var num8  =  document.getElementById("input8");       // سعر المنتج قسط 0.0
-                var num9  =  document.getElementById("input9");       //المبلغ المقسط
-                var num10 =  document.getElementById("input10");      //القسط الشهري
-                var num11 =  document.getElementById("input11");      //سعر المنتج قسط
 
-                  // سعر المنتج كاش 1 - المقدم 2 = الباقي يغد المقدم 5
-                var sub = Number(num1) - Number(num2);
-                num5.value = sub; // الباقي بعد المقدم
+      // سعر المنتج كاش 1 - المقدم 2 = الباقي يغد المقدم 5
+    var sub = Number(num1) - Number(num2);
+    num5.value = sub; // الباقي بعد المقدم
 
-                //6 النسبه / 12 = نسبة اجمالي الشهور
-                var mult = (Number(num4) / 100) / 12 * Number(num3); 
-                num6.value = mult;  //نسبة اجمالي الشهور
+    //6 النسبه / 12 = نسبة اجمالي الشهور
+    var mult = (Number(num4) / 100) / 12 * Number(num3); 
+    num6.value = mult;  //نسبة اجمالي الشهور
 
-                // الباقي بعد المقدم5 * نسبة اجمالي الشهور6 = 7نسبة الربح بالجنية 
-                var mult2 = Number(num5.value) * Number(num6.value);
-                num7.value = mult2;  //  7نسبة الربح بالجنية 
+    // الباقي بعد المقدم5 * نسبة اجمالي الشهور6 = 7نسبة الربح بالجنية 
+    var mult2 = Number(num5.value) * Number(num6.value);
+    num7.value = mult2;  //  7نسبة الربح بالجنية 
 
-                // 8نسبة الربح بالجنية 7 + سعر المنتج كاش1  = سعر المنتج قسط قبل التقريب
-                var sum = Number(num7.value) + Number(num1);
-                num8.value=sum; //8 سعر المنتج قسط قبل التقريب 
+    // 8نسبة الربح بالجنية 7 + سعر المنتج كاش1  = سعر المنتج قسط قبل التقريب
+    var sum = Number(num7.value) + Number(num1);
+    num8.value=sum; //8 سعر المنتج قسط قبل التقريب 
 
-                //  5نسبة الربح بالجنية 7 + الباقي بعد المقدم
-                var sum2 = Number(num7.value) + Number(num5.value);
-                num9.value = sum2; // 9 االمبلغ المقسط
+    //  5نسبة الربح بالجنية 7 + الباقي بعد المقدم
+    var sum2 = Number(num7.value) + Number(num5.value);
+    num9.value = sum2; // 9 االمبلغ المقسط
 
-                // المبلغ المقسط 9 / عدد الشهور3 = القسط الشهري 10
-                var div = Number(num9.value) / Number(num3);
-                num10.value = Math.floor(div); //  10 القسط الشهري 
-                // القسط الشهري 10 * عدد الشهور 3 + المقدم 2 = سعر المنتج 
-                var total = Number(num10.value) * Number(num3) + Number(num2);
-                num11.value = total ; // سعر المنتج قسط 
-              } 
+    // المبلغ المقسط 9 / عدد الشهور3 = القسط الشهري 10
+    var div = Number(num9.value) / Number(num3);
+    num10.value = Math.floor(div); //  10 القسط الشهري 
+    // القسط الشهري 10 * عدد الشهور 3 + المقدم 2 = سعر المنتج 
+    var total = Number(num10.value) * Number(num3) + Number(num2);
+    num11.value = total ; // سعر المنتج قسط 
+    localStorage.setItem("rate", num4);
+  };
+
+let gear = document.querySelector(".gear");
+let settings = document.querySelector(".settings");
+gear.addEventListener("click", function(){
+settings.classList.toggle("open")
+});
+
+
