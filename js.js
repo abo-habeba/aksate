e9f1182bcb2dbae7cba419e234f81a033020cbe5
abo-habeba <94226077+abo-habeba@users.noreply.github.com>
2022-07-12 04:09:47 +0200

    Update js.js

--- js.js
+++ js.js
@@ -55,9 +55,9 @@ function inputs(){
     localStorage.setItem("rate", num4);
 
     // share ================================================
-        let myp = `${lab2} ${num2.innerText}
-    ${lab3} ${num3.innerText}
-    ${lab10} ${num10.innerText}`;
+        let myp = `${lab2} ${num2.value}
+    ${lab3} ${num3.value}
+    ${lab10} ${num10.value}`;
     document.querySelector('.myp').innerText = myp;
 
     let shared = document.querySelector('.myp').innerText;
